0f5aaa4c95b7352b6d06d32e62233c2087204acd
Nathan Lesser <nathanlesser@outlook.com>
2016-10-14 11:40:41 -0400

    Created the first ColorSensor class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPoC.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+/**
+ * Created by Nathan on 10/14/2016.
+ */
+@Autonomous(name="Color Sensor Proof of Concept")
+public class ColorSensorPoC extends LinearOpMode{
+
+    public ColorSensorPoC() {}
+
+    public void runOpMode() {
+        r
+    }
+
+}
