01afdc58f076baafe3fc7674639b5ef8d33e24b9
Alex <bulanov416@gmail.com>
2016-10-19 16:47:24 -0400

    Pure driving 4 wheel drive code -Alex

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -18,18 +19,18 @@ public class Drive extends LinearOpMode {
     DcMotor r;
     DcMotor lb;
     DcMotor rb;
-    DcMotor fly1;
+   /* DcMotor fly1;
     DcMotor fly2;
     Servo bring;
     Servo clasp1;
     Servo clasp2;
+*/
 
-
-    public Drive () {
+    public Drive() {
 
     }
 
-    double scale_motor_power (double p_power)  //Scales joystick value to output appropriate motor power
+    double scale_motor_power(double p_power)  //Scales joystick value to output appropriate motor power
     {                                          //Use like "scale_motor_power(gamepad1.left_stick_x)"
         //
         // Assume no scaling.
@@ -39,10 +40,10 @@ public class Drive extends LinearOpMode {
         //
         // Ensure the values are legal.
         //
-        double l_power = Range.clip (p_power, -1, 1);
+        double l_power = Range.clip(p_power, -1, 1);
 
         double[] l_array =
-                { 0.00, 0.05, 0.09, 0.10, 0.12
+                {0.00, 0.05, 0.09, 0.10, 0.12
                         , 0.15, 0.18, 0.24, 0.30, 0.36
                         , 0.43, 0.50, 0.60, 0.72, 0.85
                         , 1.00, 1.00
@@ -52,21 +53,15 @@ public class Drive extends LinearOpMode {
         // Get the corresponding index for the specified argument/parameter.
         //
         int l_index = (int) (l_power * 16.0);
-        if (l_index < 0)
-        {
+        if (l_index < 0) {
             l_index = -l_index;
-        }
-        else if (l_index > 16)
-        {
+        } else if (l_index > 16) {
             l_index = 16;
         }
 
-        if (l_power < 0)
-        {
+        if (l_power < 0) {
             l_scale = -l_array[l_index];
-        }
-        else
-        {
+        } else {
             l_scale = l_array[l_index];
         }
 
@@ -74,38 +69,39 @@ public class Drive extends LinearOpMode {
 
     }
 
-    @Override public void runOpMode ()
+    @Override
+    public void runOpMode()
 
     {
-        while(true) {
+        while (true) {
 
             l = hardwareMap.dcMotor.get("l");
-        r = hardwareMap.dcMotor.get("r");
-        rb = hardwareMap.dcMotor.get("rb");
-        lb = hardwareMap.dcMotor.get("lb");
-        fly1 = hardwareMap.dcMotor.get("fly1");
+            r = hardwareMap.dcMotor.get("r");
+            rb = hardwareMap.dcMotor.get("rb");
+            lb = hardwareMap.dcMotor.get("lb");
+       /* fly1 = hardwareMap.dcMotor.get("fly1");
         fly2 = hardwareMap.dcMotor.get("fly2");
         bring = hardwareMap.servo.get("bring");
         clasp2 = hardwareMap.servo.get("clasp2");
         clasp1 = hardwareMap.servo.get("clasp1");
+*/
+            //fly2.setDirection(DcMotor.Direction.REVERSE);
+            r.setDirection(DcMotor.Direction.REVERSE);
+            rb.setDirection(DcMotor.Direction.REVERSE);
 
-        fly2.setDirection(DcMotor.Direction.REVERSE);
-        r.setDirection(DcMotor.Direction.REVERSE);
-        rb.setDirection(DcMotor.Direction.REVERSE);
+            float l_gp1_left_stick_y = gamepad1.left_stick_y;
+            float l_left_drive_power
+                    = (float) scale_motor_power(l_gp1_left_stick_y);
 
-        float l_gp1_left_stick_y = gamepad1.left_stick_y;
-        float l_left_drive_power
-                = (float) scale_motor_power(l_gp1_left_stick_y);
+            float l_gp1_right_stick_y = gamepad1.right_stick_y;
+            float l_right_drive_power
+                    = (float) scale_motor_power(l_gp1_right_stick_y);
 
-        float l_gp1_right_stick_y = gamepad1.right_stick_y;
-        float l_right_drive_power
-                = (float) scale_motor_power(l_gp1_right_stick_y);
-
-        r.setPower(l_right_drive_power);
-        l.setPower(l_left_drive_power);
-        rb.setPower(l_right_drive_power);
-        lb.setPower(l_left_drive_power);
-            while (gamepad1.dpad_up) {
+            r.setPower(l_right_drive_power);
+            l.setPower(l_left_drive_power);
+            rb.setPower(l_right_drive_power);
+            lb.setPower(l_left_drive_power);
+          /*  while (gamepad1.dpad_up) {
                 fly1.setPower(1);
                 fly2.setPower(1);
             }
@@ -150,7 +146,7 @@ public class Drive extends LinearOpMode {
         sleep(2000);
         clasp1.setPosition(0.5);
         clasp2.setPosition(0.5);*/
-    }
+        }
 
         //This is the function to pick up balls and bring them to the launcher
        /* public void ball() {
@@ -165,8 +161,9 @@ public class Drive extends LinearOpMode {
 
     }
 */
-    //This function should be used for sleep because it is setup to throw InterruptedException.
+        //This function should be used for sleep because it is setup to throw InterruptedException.
+
+    }
+
 
 }
-
-
