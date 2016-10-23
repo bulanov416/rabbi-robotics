8c2b5f55213a1ae59ddfbbebd54c5d504c3db128
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-10-23 15:03:29 -0400

    Create simple up-down teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UpDown.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by Matan on 10/23/2016.
+ */
+@TeleOp(name = "Up Down")
+public class UpDown extends LinearOpMode {
+    DcMotor motor;
+    public void runOpMode() {
+        gamepad1.setJoystickDeadzone((float) 0.05);
+        motor = hardwareMap.dcMotor.get("motor");
+        while (true) {
+            float power = Range.clip(gamepad1.left_stick_y, -1, 1);
+            motor.setPower(power);
+        }
+    }
+}
