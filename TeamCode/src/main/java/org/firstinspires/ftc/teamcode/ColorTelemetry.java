99090e034fc42c4ae61c70a483cfbedb3053c436
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-10-23 13:57:29 -0400

    Prettify telemetry data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTelemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTelemetry.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
+import org.firstinspires.ftc.robotcontroller.internal.testcode.TestColorSensors;
+
 /**
  * Created by Matan on 10/23/2016.
  */
@@ -14,7 +16,11 @@ public class ColorTelemetry extends LinearOpMode {
     public void runOpMode() {
         while (true) {
             colorSensor = hardwareMap.colorSensor.get("color");
-            telemetry.addData("output", colorSensor);
+            telemetry.addData("red", colorSensor.red());
+            telemetry.addData("blue", colorSensor.blue());
+            telemetry.addData("green", colorSensor.green());
+            telemetry.addData("alpha", colorSensor.alpha());
+            telemetry.addData("all together now", colorSensor.argb());
             telemetry.update();
         }
     }
