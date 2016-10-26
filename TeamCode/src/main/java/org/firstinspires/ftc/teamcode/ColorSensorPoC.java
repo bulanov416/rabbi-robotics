4862ff965b93b19350baaefba67f91558d5e566e
Nathan Lesser <nathanlesser@outlook.com>
2016-10-26 12:52:19 -0400

    Revert master branch to pre-organization state
    The organization is still in a branch called "levi-organization"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPoC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPoC.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.debug;
+package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Color;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoMovePoC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoMovePoC.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.debug;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
