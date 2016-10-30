fda506b267319117c2fcb92aba4e1ff5dbbfabbc
Levi <legof4@outlook.com>
2016-10-29 20:22:30 -0400

    Further Logger development This one was written with UNIX file structure in mind, and should operate so long as the FTC app has the appropriate permissions. Should this not be the case, will attempt to print error to console.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/libraries/AndroidLogger.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.libraries;
+
+/**
+ * Created by Levi on 10/29/2016.
+ */
+public class AndroidLogger {
+    public AndroidLogger () {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/libraries/FileLogger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/libraries/FileLogger.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import android.os.Environment;
 
 /**
  * Created by Levi on 10/23/2016.
@@ -17,28 +18,45 @@ public class FileLogger {
      * in its name. To use, instantiate object FileLogger with desired "name". Do not include .txt!
      **/
     private String name;
-    private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm MM/dd/yyyy");
+    private SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+    private SimpleDateFormat sdf2 = new SimpleDateFormat("MM-dd-yyyy--HH:mm:ss");
+    private String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).toString()+"/4466Logs";
 
     public FileLogger(String name) {
         /**
          * Constructor.
          * @param name (required) The name of the output file, without formatting info
          */
-        this.name = name + System.currentTimeMillis();
-        String path = name + ".txt";
-        File file = new File(path);
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Hopefully this prints");
 
+        File dirStorage = new File(path);
+        if (dirStorage.canWrite()) {
+            if (!dirStorage.isDirectory()) {
+                dirStorage.mkdir();
             }
+
+            this.name = name + sdf2.format(System.currentTimeMillis()) + ".txt";
+
+            File file = new File(dirStorage, name);
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) { e.printStackTrace(); }
+            }
+            else {
+                file.delete();
+                try {
+                    file.createNewFile(); } catch (IOException e) { e.printStackTrace();
+                }
+            }
+
+            this.write("Name: " + this.name); // This should be left, helps to make the log file appear organized and legible.
+            this.write(System.getProperty("line.separator") + "Launched at: " + sdf.format(System.currentTimeMillis()));
+            this.write(System.getProperty("line.separator") + "<----------------------------------------------->");
         }
-        this.write("Name: " + this.name); // This should be left, helps to make the log file appear organized and legible.
-        this.write(System.getProperty("line.separator") + "Launched at: " + sdf.format(System.currentTimeMillis()));
-        this.write(System.getProperty("line.separator") + "<----------------------------------------------->");
+        else {
+            System.out.println("Error - Insufficient Permissions to edit programmed directory.");
+            throw new RuntimeException();
+        }
     }
 
 
@@ -47,10 +65,8 @@ public class FileLogger {
     **/
     public void write(String line){
         try {
-            String fpath = name+".txt";
-            File file = new File(fpath);
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
+            File file = new File(path, name);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
             String time = sdf.format(System.currentTimeMillis());
             line = System.getProperty("line.separator") + "[" + time + "]: " + line;
             bw.write(line);
