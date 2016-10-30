ed52fcf6e3a6010c8a7f5670fdbed0b544cce6b8
Nathan Lesser <nathanlesser@outlook.com>
2016-10-30 13:33:08 -0400

    Added full functionality to HardwareRabbi

--- TeamCode/src/main/java/org/gannacademy/libraries/HardwareRabbi.java
+++ TeamCode/src/main/java/org/gannacademy/libraries/HardwareRabbi.java
@@ -1,8 +1,19 @@
 package org.gannacademy.libraries;
 
+import com.qualcomm.hardware.adafruit.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.TelemetryInternal;
+
+/*
+ *
+ */
 
 /**
  * This is NOT an opmode.
@@ -12,11 +23,13 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * This hardware class assumes the following device names have been configured on the robot:
  * Note:  All names are lower case and some have single spaces between words.
  *
- * Motor channel:  Left  drive motor:        "left_drive"
- * Motor channel:  Right drive motor:        "right_drive"
- * Motor channel:  Manipulator drive motor:  "left_arm"
- * Servo channel:  Servo to open left claw:  "left_hand"
- * Servo channel:  Servo to open right claw: "right_hand"
+ * Motor: Left  drive motor:        "l"
+ * Motor: Right drive motor:        "r"
+ * Motor: Left back drive motor:    "lb"
+ * Motor: Right Back Drive Motor:   "rb"
+ * Servo: Button Pusher:            "buttonPushServo"
+ *
+ * More motors can be added as they are implemented
  */
 public class HardwareRabbi
 {
@@ -25,44 +38,51 @@ public class HardwareRabbi
     public DcMotor r;
     public DcMotor lb;
     public DcMotor rb;
+    public Servo   buttonPushServo;
 
     /* local OpMode members. */
-    HardwareMap hwMap           =  null;
+    HardwareMap hwMap;
     private ElapsedTime period  = new ElapsedTime();
 
-    /* Constructor */
-    public HardwareRabbi(){
+    /* Enum ButtonPushPositions {
+        LEFT_BUTTON, REST_POSITION, RIGHT_BUTTON;
+    }; */
 
-    }
+    /* Constructor */
+    public HardwareRabbi(){}
 
     /* Initialize standard Hardware interfaces */
     public void init(HardwareMap ahwMap) {
-        // Save reference to Hardware map
-        hwMap = ahwMap;
+        hwMap = ahwMap; // uses the HardwareMap from the OpMode
 
         // Define and Initialize Motors
         l  = hwMap.dcMotor.get("l");
-        r  = hwMap.dcMotor.get("r");
         lb = hwMap.dcMotor.get("lb");
+        r  = hwMap.dcMotor.get("r");
         rb = hwMap.dcMotor.get("rb");
 
         l .setDirection(DcMotor.Direction.FORWARD);
         lb.setDirection(DcMotor.Direction.FORWARD);
         r .setDirection(DcMotor.Direction.REVERSE);
         rb.setDirection(DcMotor.Direction.FORWARD);
-        // Set all motors to zero power
-        l.setPower(0);
-        r.setPower(0);
+        // TODO replace code below with stopDriving() method
+        l. setPower(0);
+        lb.setPower(0);
+        r. setPower(0);
+        rb.setPower(0);
+        // Make sure the motors are not expecting encoders
+        l .setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        r .setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-
-        // Set all motors to run without encoders.
-        // May want to use RUN_USING_ENCODERS if encoders are installed.
-        l.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        r.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Initialize the servo
+        buttonPushServo = hwMap.servo.get("buttonPushServo");
+        // Set it's position
+        buttonPushServo.setPosition(90); // replace with vars
     }
 
     /***
-     *
      * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
      * periodic tick.  This is used to compensate for varying processing times for each cycle.
      * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
@@ -80,6 +100,50 @@ public class HardwareRabbi
 
         // Reset the cycle clock for the next pass.
         period.reset();
+    }
+
+    /***
+     * Drives the robot for a certain distance, defined by centimeters. To figure out how long to
+     * run the motors, a conversion factor between centimeters and seconds is applied. Negative
+     * distances move the robot backwards.
+     *
+     * @param distance  the distance to drive in Centimeters.
+     * @throws InterruptedException
+     */
+    public void driveCentimeters (double distance, double powerPercentage) throws InterruptedException {
+        // drives a distance in cm based on a conversion factor from seconds
+        double conversionFactor = 0; // this needs to be filled
+        double powerConversionFactor = powerPercentage / 100; // for converting power
+        double distanceInSeconds = distance / conversionFactor;
+        driveSeconds(100, distanceInSeconds * powerConversionFactor);
+        // in the future, we should set up a way to drive at lower powers
+    }
+
+    /***
+     * Drives the robot for a certain amount of time, at a certain power. Time is in Seconds, power is
+     * between 1 and -1. Negative power drives the robot backwards.
+     *
+     * @param power  the power to run the motors at (between -1 and 1)
+     * @param time  the amount of seconds to run the motors for (negative number)
+     * @throws InterruptedException
+     */
+    public void driveSeconds(double power, double time) throws InterruptedException {
+        if (time < 0) {
+            System.exit(1); // crash with exit code 1, which means an error occured
+        }
+        r.setPower(power);
+        rb.setPower(power);
+        l.setPower(power);
+        lb.setPower(power);
+        Thread.sleep((long) time * 100);
+        stopDriving();
+    }
+
+    public void stopDriving() {
+        r.setPower(0);
+        rb.setPower(0);
+        l.setPower(0);
+        lb.setPower(0);
     }
 }
 
