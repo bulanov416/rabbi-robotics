1cc65e4a9ad6fdb9028d371673a50c813e9b92b2
Nathan <nlesser16@gmail.com>
2016-11-01 23:38:32 -0400

    Added an untested Android-Friendly logger
    I also updated Levi's FileLoggerDemo to use this logger. Micah, I hope
    you're paying attention.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug/FileLoggerAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug/FileLoggerAuto.java
@@ -3,7 +3,11 @@ package org.firstinspires.ftc.teamcode.debug;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import com.qualcomm.robotcore.util.ThreadPool;
+import org.gannacademy.libraries.ContextFileLogger;
 import org.gannacademy.libraries.FileLogger;
+
+import java.io.IOException;
 
 /**
  * Created by Levi on 11/1/2016.
@@ -14,9 +18,29 @@ public class FileLoggerAuto extends LinearOpMode{
     public FileLoggerAuto() {}
 
     @Override
-    public void runOpMode() {
-        FileLogger logger = new FileLogger("TestLog");
-        logger.write("Init 1");
+    public void runOpMode() throws InterruptedException {
+        ContextFileLogger logger = null;
+        try {
+            logger = new ContextFileLogger("TestLog");
+        } catch (IOException e) {
+            e.printStackTrace();
+            telemetry.addData("LOGGER FAILED", "IOException thrown. Wait 10s for stop");
+            telemetry.update();
+            Thread.sleep(1000);
+            stop();
+        }
+
+        logger.write("Hey, can you hear me?");
+        logger.write("Thought so.");
+
+        waitForStart();
+
+        logger.write("Is there anything wrong with your life?");
+        logger.write("You can fix it with GRACIOUS PROFESSIONALISM(tm).");
+        logger.write("Warning: side effects may transformation into Micah.");
+        Thread.sleep(1000);
+        logger.write("GRACIOUS PROFESSIONALISM(tm): not even once.");
+        stop();
     }
 
 }
