e2eb7f89781e09d8d1444dc78303c7d88cbdc4a6
Alex <bulanov416@gmail.com>
2017-01-05 23:14:51 -0500

    Thursday night changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedCompetition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRedCompetition.java
@@ -1,14 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
-
 
 
 /**
@@ -23,7 +20,6 @@ public class AutonomousRedCompetition extends LinearOpMode {
     DcMotor lb;
     DcMotor rb;
     OpticalDistanceSensor eods;
-    ColorSensor color_left;
     ColorSensor color_right;
     Servo button_left;
     Servo button_right;
@@ -47,27 +43,33 @@ public class AutonomousRedCompetition extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
+            //Sets Initial Servo Positions
             wall_servo.setPosition(0.39);
             button_right.setPosition(0.1);
             button_left.setPosition(0.9);
             color_right.enableLed(false);
+            //Moves to Line from Start
             while (eods.getLightDetected() < 0.03 && opModeIsActive()) {
                 drive(0.2);
             }
             if (!opModeIsActive()) break;
             stopDrive();
+            //Drives slightly past Line
             drive(0.2);
             sleepOpMode(100);
             stopDrive();
+            //Turns left onto Line
             while (eods.getLightDetected() < 0.03 && opModeIsActive()) {
                 setLeftPower(0.2);
                 setRightPower(-0.2);
             }
             if (!opModeIsActive()) break;
+            //Turns until past Line to follow Left edge of Line
             while (eods.getLightDetected() > 0.03 && opModeIsActive()) {
             }
             if (!opModeIsActive()) break;
             stopDrive();
+            //Wiggle Line-Follower
             while (!touch.isPressed()) {
                 while (eods.getLightDetected() < 0.03 && !touch.isPressed() && opModeIsActive()) {
                     setRightPower(0.14);
@@ -82,60 +84,74 @@ public class AutonomousRedCompetition extends LinearOpMode {
             }
             stopDrive();
             if (!opModeIsActive()) break;
-            telemetry.addLine("B/R : " + Integer.toString(color_right.red()) + "/" + Integer.toString(color_right.blue()));
-            telemetry.update();
+            //Gets First's Beacon color, true if red, false if blue
             boolean colorFirstSide = RightRed();
-            //insert beacon pushing code here
+            telemetry.addLine("Color Right First: " + Boolean.toString(colorFirstSide));
+            telemetry.update();
+            //Drives back from Beacon
             drive(-0.12);
             sleepOpMode(600);
             if (!opModeIsActive()) break;
             stopDrive();
+            //Retracts button
             wall_servo.setPosition(0.1);
-            sleepOpMode(550);
             if (!opModeIsActive()) break;
+            //Deploys pusher servos
             if (!colorFirstSide) {
                 button_right.setPosition(0.95);
             } else {
                 button_left.setPosition(0.05);
             }
+            //Waits for servos to move
+            sleepOpMode(550);
+            //Turns and moves forward until crosses left edge of Line
             while (eods.getLightDetected() > 0.03 && opModeIsActive()) {
                 setRightPower(0.12);
             }
             if (!opModeIsActive()) break;
             stopDrive();
+            //Drives forward and presses button
             drive(0.13);
             sleepOpMode(700);
             if (!opModeIsActive()) break;
             stopDrive();
-            //Second Beacon
+            //SECOND BEACON
+            //Drives Back
             drive(-0.2);
             sleepOpMode(400);
             stopDrive();
             if (!opModeIsActive()) break;
+            //Sets Servos
             wall_servo.setPosition(0.37);
             button_right.setPosition(0.1);
             button_left.setPosition(0.9);
+            //Right turn
             setLeftPower(-0.18);
             setRightPower(0.18);
             sleepOpMode(1700);
             if (!opModeIsActive()) break;
+            //Drives until second Line
             while (eods.getLightDetected() < 0.03 && opModeIsActive()) {
                 drive(0.15);
             }
             if (!opModeIsActive()) break;
             stopDrive();
+            //Drives slightly past Line
             drive(0.2);
             sleepOpMode(100);
             stopDrive();
+            //Turns left until encounters Line
             while (eods.getLightDetected() < 0.03 && opModeIsActive()) {
                 setLeftPower(0.18);
                 setRightPower(-0.18);
             }
             if (!opModeIsActive()) break;
+            //Continues until passes Line
             while (eods.getLightDetected() > 0.03 && opModeIsActive()) {
             }
             if (!opModeIsActive()) break;
             stopDrive();
+            //Wiggle Line-Follower
             while (!touch.isPressed()) {
                 while (eods.getLightDetected() < 0.03 && !touch.isPressed() && opModeIsActive()) {
                     setRightPower(0.17);
@@ -148,34 +164,53 @@ public class AutonomousRedCompetition extends LinearOpMode {
                 if (!opModeIsActive()) break;
                 stopDrive();
             }
-
             if (!opModeIsActive()) break;
+            //Gets Second Beacon right color, true if red
             boolean colorSecondSide = RightRed();
+            telemetry.addLine("Color Right Second: " + Boolean.toString(colorFirstSide));
+            telemetry.update();
             stopDrive();
-            setRightPower(0.17);
-            setRightPower(-0.17);
-            sleepOpMode(300);
-            stopDrive();
-            //insert beacon pushing code here
+            //Drives Back
             drive(-0.12);
             sleepOpMode(600);
+            //Left Turn, minor
+            setRightPower(-0.17);
+            setLeftPower(0.17);
+            sleepOpMode(300);
+            stopDrive();
             if (!opModeIsActive()) break;
             stopDrive();
+            //Retracts wall servo
             wall_servo.setPosition(0.1);
-            sleepOpMode(550);
             if (!opModeIsActive()) break;
+            //Deploys button pushers
             if (!colorSecondSide) {
                 button_right.setPosition(0.95);
             } else {
                 button_left.setPosition(0.05);
             }
-            while (eods.getLightDetected() > 0.03 && opModeIsActive()) {
-                setRightPower(0.17);
+            //Waits for Servos to deploy
+            sleepOpMode(550);
+            //Final adjustments, depending on position relative to line
+            if (eods.getLightDetected() > 0.03) {
+                while (eods.getLightDetected() > 0.03 && opModeIsActive()) {
+                    setLeftPower(0.17);
+                }
+            }
+            else {
+                while (eods.getLightDetected() < 0.03 && opModeIsActive()) {
+                    setRightPower(0.17);
+                }
             }
             if (!opModeIsActive()) break;
             stopDrive();
+            //Drives forward, presses button
             drive(0.13);
             sleepOpMode(700);
+            stopDrive();
+            //Drives back
+            drive(-0.13);
+            sleepOpMode(400);
             stopDrive();
             break;
         }
@@ -222,12 +257,24 @@ public class AutonomousRedCompetition extends LinearOpMode {
         }
     }
 
-    public boolean RightRed() throws InterruptedException{
-        if (color_right.red() < 200 && color_right.blue() < 200) {
+    public boolean RightRedI() throws InterruptedException{
+        if (color_right.red() < 5.1 && color_right.blue() < 5.1 && color_right.blue() != color_right.red()) {
             return color_right.red() > color_right.blue();
         }
         else {
             sleepOpMode(1);
+            return RightRedI();
+        }
+    }
+
+    public boolean RightRed() throws InterruptedException{
+        boolean redI = RightRedI();
+        sleepOpMode(150);
+        if (redI == RightRedI()) {
+            return redI;
+        }
+        else {
+            sleepOpMode(150);
             return RightRed();
         }
     }
